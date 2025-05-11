ee6118481bde70d8d89ea3f7166b2212398439ad
K Alisha Patra <127962744+Alishapatr@users.noreply.github.com>
2025-05-11 09:12:35 +0530

    Resolved merge conflict in OddEven.py

---
+++ OddEven.py
@@ -0,0 +1,13 @@
+def check_even_or_odd(num):
+if num % 2 == 0:
+return "Even"
+else:
+return "Odd"
+<<<<<<< Updated upstream
+print(check_even_or_odd(7)) 
+
+
+
+=======
+print(check_even_or_odd(7)) 
+>>>>>>> Stashed changes
